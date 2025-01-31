b4ca451ec5c5c6022e4b4cb28e1df0304d510813
Mikehope254 <mikehope1738@gmail.com>
2025-01-31 12:15:13 +0300

    Added app.put, app.get and app.delete

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the req.body (middleware)
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
@@ -25,8 +36,40 @@ app.post("/api/products", async (req, res) => {
     await newProduct.save();
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
-    console.error("Error in Creating Product", error.message);
+    console.log("Error in Creating Product", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    //to check if input is valid MongoDB ObjectID
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product Deleted" });
+  } catch (error) {
+    console.log("error in deleting products", error.message);
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
