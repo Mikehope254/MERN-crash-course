13bef01898f19149952aee14f4c92068b34003af
Mikehope254 <mikehope1738@gmail.com>
2025-02-11 10:11:00 +0300

    set

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -3,8 +3,11 @@ import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
   products: [],
+
   setProducts: (products) => set({ products }),
+
   createProduct: async (newProduct) => {
+    //validate inputs
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: "Please fill in all fields" };
     }
